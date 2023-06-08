376aee14650b2b80953d29c4fe22ac99c318169f
lihikapsi <lihi.kaspi@campus.technion.ac.il>
2023-06-08 13:48:21 +0300

    only opened classes

---
+++ src/ArrayStack.java
@@ -0,0 +1,7 @@
+public class ArrayStack<E> implements Stack<E>{
+    private int maxCapacity;
+
+    public ArrayStack<E>(int maxCapacity) {
+
+    }
+}
---
+++ src/Song.java
@@ -0,0 +1,11 @@
+public class Song {
+
+    public enum Genre {
+        POP,
+        ROCK,
+        HIP_HOP,
+        COUNTRY,
+        JAZZ,
+        DISCO
+    }
+}
